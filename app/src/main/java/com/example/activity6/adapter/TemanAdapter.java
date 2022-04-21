ee6e588f57006e78ccb861eb41032ce7331b123a
atafatta <atafatta@gmail.com>
2022-04-21 21:14:43 +0700

    Activity 8 || Menyimpan data dan membaca data menggunakan database MySQL

--- app/src/main/java/com/example/activity6/adapter/TemanAdapter.java
+++ app/src/main/java/com/example/activity6/adapter/TemanAdapter.java
@@ -1,19 +1,27 @@
 package com.example.activity6.adapter;
 
+import android.content.Context;
+import android.content.Intent;
 import android.graphics.Color;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.PopupMenu;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.activity6.MainActivity;
+import com.example.activity6.edit_teman;
 import com.example.activity6.R;
+import com.example.activity6.database.DBController;
 import com.example.activity6.database.Teman;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class TemanAdapter extends RecyclerView.Adapter<TemanAdapter.TemanViewHolder> {
     private ArrayList<Teman> listData;
@@ -22,25 +30,74 @@ public class TemanAdapter extends RecyclerView.Adapter<TemanAdapter.TemanViewHol
         this.listData = listData;
     }
 
+    private Context control;
+
+    public TemanAdapter.TemanViewHolder onCreateTemanViewHolder(ViewGroup parent, int viewType){
+        LayoutInflater layoutInf = LayoutInflater.from(parent.getContext());
+        View v = layoutInf.inflate(R.layout.row_data_teman,parent,false);
+        control = parent.getContext();
+        return new TemanViewHolder(v);
+    }
+
+
+
     @Override
     public TemanViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        LayoutInflater layoutInf = LayoutInflater.from(parent.getContext());
-        View view = layoutInf.inflate(R.layout.row_data_teman,parent,false);
+        LayoutInflater layoutinf = LayoutInflater.from(parent.getContext());
+        View view = layoutinf.inflate(R.layout.row_data_teman,parent,false);
+        control = parent.getContext();
         return new TemanViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull TemanViewHolder holder, int position) {
-        String nm,tlp;
+    public void onBindViewHolder(TemanAdapter.TemanViewHolder holder, int position) {
+        String nm,tlp,id;
 
+        id= listData.get(position).getId();
         nm = listData.get(position).getNama();
         tlp = listData.get(position).getTelpon();
+        DBController db = new DBController(control);
 
-        holder.namaTxt.setTextColor(Color.BLUE);
+
         holder.namaTxt.setTextSize(20);
+        holder.namaTxt.setTextColor(Color.BLUE);
         holder.namaTxt.setText(nm);
         holder.telponTxt.setText(tlp);
 
+        holder.cardku.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                PopupMenu popupMenu = new PopupMenu(control,holder.cardku);
+                popupMenu.inflate(R.menu.menu);
+                popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem item) {
+                        switch (item.getItemId()){
+                            case R.id.mnEditnew:
+                                Intent i = new Intent(control,edit_teman.class);
+                                i.putExtra("id",id);
+                                i.putExtra("nama",nm);
+                                i.putExtra("telpon",tlp);
+                                control.startActivity(i);
+                                break;
+
+                            case R.id.mnHapusnew:
+                                HashMap<String,String> values = new HashMap<>();
+                                values.put("id",id);
+                                db.DeleteData(values);
+                                Intent j = new Intent(control, MainActivity.class);
+                                control.startActivity(j);
+                                break;
+                        }
+
+                        return true;
+                    }
+                });
+                popupMenu.show();
+
+                return false;
+            }
+        });
     }
 
     @Override
---
+++ app/src/main/java/com/example/activity6/edit_teman.java
@@ -0,0 +1,67 @@
+package com.example.activity6;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.example.activity6.database.DBController;
+import com.google.android.material.textfield.TextInputEditText;
+
+import java.util.HashMap;
+
+public class edit_teman extends AppCompatActivity {
+    TextInputEditText Nama,Telpon;
+
+    Button Save;
+
+    String nama,telp,id;
+
+    DBController controller = new DBController(edit_teman.this);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_teman2);
+
+        Nama = findViewById(R.id.edtNama);
+        Telpon = findViewById(R.id.edtTelp);
+        Save = findViewById(R.id.ubahButton);
+
+        id = getIntent().getStringExtra("id");
+        nama = getIntent().getStringExtra("nama");
+        telp = getIntent().getStringExtra("telpon");
+
+        setTitle("Edit Data");
+        Nama.setText(nama);
+        Telpon.setText(telp);
+
+        Save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(Nama.getText().toString().isEmpty() || Telpon.getText().toString().isEmpty()){
+                    Toast.makeText(getApplicationContext(),"Data Tidak Boleh Kosong",Toast.LENGTH_LONG).show();
+                }else{
+                    nama = Nama.getText().toString();
+                    telp = Telpon.getText().toString();
+                    HashMap<String,String> values = new HashMap<>();
+                    values.put("id",id);
+                    values.put("nama",nama);
+                    values.put("telpon",telp);
+                    controller.UpdateData(values);
+                    callHome();
+                }
+
+            }
+        });
+    }
+
+    public void callHome(){
+        Intent i = new Intent(edit_teman.this,MainActivity.class);
+        startActivity(i);
+        finish();
+    }
+}
